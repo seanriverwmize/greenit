155c0a72fed6cebd75d205f76c84e1ddbc82eba4
Sean River <seanriverwmize@gmail.com>
2019-01-31 22:22:55 -0800

    added user_id index to link model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_31_041627) do
+ActiveRecord::Schema.define(version: 2019_02_01_062042) do
 
   create_table "links", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
